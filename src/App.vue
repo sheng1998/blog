dde4a287fcf08bf1dee9a5b2571533f25b1ad9fa
z <18475140601@163.com>
2022-05-02 00:10:41 +0800

    feat: 项目初始化

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <span>Vue3 + Vite + Elemen-Plus</span>
+    <el-button>我是 Element-Plus 的按钮</el-button>
+    <el-card>
+      {{ message }}
+    </el-card>
+    <div class="footer"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const message = ref<number | string>(1);
+console.log(message.value);
+</script>
+
+<style lang="scss">
+#app {
+  .footer {
+    width: 100%;
+    height: 200px;
+    background-color: pink;
+  }
+}
+</style>
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+
+import '$style/normalize.css';
+import '$style/global.scss';
+
+createApp(App).mount('#app');
