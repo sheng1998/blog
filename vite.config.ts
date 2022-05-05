cf1b455900d06f732657a61b45d0d3cd2bd63baf
z <18475140601@163.com>
2022-05-05 22:59:38 +0800

    chore: 设置不自动引入src/components里面的组件

--- vite.config.ts
+++ vite.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
     Components({
       // dirs 指定组件所在位置，默认为 src/components
       // 可以让我们使用自己定义组件的时候免去 import 的麻烦
-      // dirs: ['src/components/'],
+      dirs: [],
       // 配置需要将哪些后缀类型的文件进行自动按需引入
       extensions: ['vue'],
       // 解析的 UI 组件库(Element-Plus)
